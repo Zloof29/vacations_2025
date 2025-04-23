fc6cd2002487c882a4e653a1a34fbebadb994ad3
Lior Bas <look551@gmail.com>
2025-04-23 22:11:46 +0300

    trying to fix image error

--- Backend/src/2-utils/app-config.ts
+++ Backend/src/2-utils/app-config.ts
@@ -7,7 +7,8 @@ class AppConfig {
   public readonly isDevelopment = process.env.ENVIRONMENT === "development";
   public readonly isProduction = process.env.ENVIRONMENT === "production";
   public readonly port = process.env.PORT;
-  public readonly mongodbConnectionString = process.env.MONGODB_CONNECTION_STRING;
+  public readonly mongodbConnectionString =
+    process.env.MONGODB_CONNECTION_STRING;
   public readonly jwtSecretKey = process.env.JWT_SECRET_KEY;
   public readonly passwordSalt = process.env.PASSWORD_SALT;
   public readonly baseImageUrl = process.env.BASE_IMAGE_URL;
